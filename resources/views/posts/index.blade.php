da5c20876bb77ecc7adce1c64d371428c1ed3bd5
oybekalloyorov <alloyorovoybek1997@gmail.com>
2025-08-05 17:09:56 +0500

    Paginatsiya - pagination ni o'rganish

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -39,9 +39,9 @@
                         <a class="btn btn-sm btn-primary py-2" href="{{ route('posts.show', ['post' => $post->id ]) }}">O'qish</a>
                     </div>
                 @endforeach
+                    {{$posts->links()}}
 
-
-                <div class="col-12">
+                {{-- <div class="col-12">
                     <nav aria-label="Page navigation">
                       <ul class="pagination pagination-lg justify-content-center mb-0">
                         <li class="page-item disabled">
@@ -61,7 +61,7 @@
                         </li>
                       </ul>
                     </nav>
-                </div>
+                </div> --}}
             </div>
         </div>
     </div>
