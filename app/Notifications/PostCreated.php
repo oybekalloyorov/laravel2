925ec8b4deb84b68fe08c88f4749eed976548ad7
oybekalloyorov <alloyorovoybek1997@gmail.com>
2025-08-08 12:40:20 +0500

    Notifications - Xabarnomalar queue

--- app/Notifications/PostCreated.php
+++ app/Notifications/PostCreated.php
@@ -8,7 +8,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class PostCreated extends Notification
+class PostCreated extends Notification implements ShouldQueue
 {
     use Queueable;
     public $post;
